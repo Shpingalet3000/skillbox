c7c66f0412379ffe54eca903cb03d444b449a471
Екатерина Канарейкина <ekaterina.kanareykina@lab-systems.ru>
2023-08-25 12:54:51 +0300

    Work completed in module 14

--- 04_coin2/main.py
+++ 04_coin2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def check_coin(X, Y, r):
+    distance = (X ** 2 + Y ** 2) ** 0.5
+    if distance <= r:
+        print("\nМонетка где-то рядом")
+    else:
+        print("\nМонетки в области нет")
+
+print ("Введите координаты монетки:")
+coordinate_X = float(input("X: "))
+coordinate_Y = float(input("Y: "))
+radius = float(input("Введите радиус: "))
+
+check_coin(coordinate_X, coordinate_Y, radius)
--- 05_years/main.py
+++ 05_years/main.py
@@ -1,1 +1,49 @@
-# TODO здесь писать код
+def check_special_years(first_year, second_year):
+    for year in range(first_year, second_year + 1):
+        digits = str(year)
+        if digits[0] == digits[1] == digits[2]:
+            print(year)
+        elif digits[1] == digits[2] == digits[3]:
+            print(year)
+        elif digits[0] == digits[2] == digits[3]:
+            print(year)
+        elif digits[0] == digits[1] == digits[3]:
+            print(year)
+
+
+print("Первый вариант решения задачи")
+first_year = int(input("Введите первый год: "))
+second_year = int(input("Введите второй год: "))
+print("\nГоды от", first_year, "до", second_year, "с тремя одинаковыми цифрами:")
+special_years = check_special_years(first_year, second_year)
+
+
+def check_special_years(first_year, second_year):
+    special_years = []
+    for year in range(first_year, second_year + 1):
+        digits = [int(digit) for digit in str(year)]
+        if digits[0] == digits[1] == digits[2]:
+            special_years.append(year)
+        elif digits[1] == digits[2] == digits[3]:
+            special_years.append(year)
+        elif digits[0] == digits[2] == digits[3]:
+            special_years.append(year)
+        elif digits[0] == digits[1] == digits[3]:
+            special_years.append(year)
+    return special_years
+
+
+print("\nВторой вариант решения задачи")
+first_year = int(input("Введите первый год: "))
+second_year = int(input("Введите второй год: "))
+
+special_years = check_special_years(first_year, second_year)
+if len(special_years) != 0:
+    print("\nГоды от", first_year, "до", first_year, "с тремя одинаковыми цифрами:")
+    for year in special_years:
+        print(year)
+else:
+    print("\nВо временном интервале " + str(first_year) + "-" + str(
+        first_year) + " гг. нет года с тремя одинаковыми цифрами.")
+
+
