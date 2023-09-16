97df2854bdb2fa832a7952ab9e8238afa8c04465
kanareykina.e.s <kanareykina.e.s@yandex.ru>
2023-09-16 23:10:47 +0300

    Module 16 assignments completed.

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,1 +1,15 @@
-# TO DO 
+main_list = [1, 5, 3]
+first_secondary_list = [1, 5, 1, 5]
+second_secondary_list = [1, 3, 1, 5, 3, 3]
+
+main_list.extend(first_secondary_list)
+print("Количество цифр 5 при первом объединении:", main_list.count(5))
+
+for i_element in main_list:
+    if i_element == 5:
+        main_list.remove(i_element)
+
+main_list.extend(second_secondary_list)
+print("Количество цифр 3 при втором объединении:", main_list.count(3))
+print("Итоговый список:", main_list)
+
--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,1 +1,22 @@
-# TO DO 
+guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+
+while True:
+    print('\nСейчас на вечеринке', len(guests), 'гостей', guests)
+    command = input('Гость пришел или ушел? ')
+    if command == 'пришел':
+        name = input('Имя гостя: ')
+        if len(guests) < 6:
+            print('Привет,', name + '!')
+            guests.append(name)
+        else:
+            print('Прости, ' + name + ', но мест нет.')
+    elif command == 'ушел':
+        name = input('Имя гостя: ')
+        if name in guests:
+            print('Пока,', name + '!')
+            guests.remove(name)
+        else:
+            print('Такого гостя на вечеринке нет.')
+    elif command == 'Пора спать':
+        print('Вечеринка закончилась, все легли спать.')
+        break
